ec2ce882589dfed7c17a12fd8a3af7ec83e72136
inthrakumar <inthrak04@gmail.com>
2022-12-22 20:09:52 +0530

    merge thing

--- codes/java/a.java
+++ codes/java/a.java
@@ -1,28 +1,19 @@
-public class a{
+public class a {// got it
     public static void main(String[] args) {
-        int st=0;
-        for(int i=1;i<=9;i++){
-            if(i>5){
+        int st = 0;
+        for (int i = 1; i <= 9; i++) {
+            if (i > 5) {
                 --st;
-            }else{
+            } else {
                 ++st;
             }
 
-            for(int j=0;j<st;j++){
+            for (int j = 0; j < st; j++) {
                 System.out.print("* ");
             }
             System.out.println(" ");
-            
-
 
         }
-   }
+    }
 
-        
-      
-        
-
-
-        
-    
 }
