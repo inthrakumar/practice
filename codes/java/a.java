1b1b9fea2229536f97462e2dc11cb154b06f11e7
inthrakumar <inthrak04@gmail.com>
2022-12-22 19:52:08 +0530

    got it

--- codes/java/a.java
+++ codes/java/a.java
@@ -1,21 +1,19 @@
 public class a {
     public static void main(String[] args) {
-        int st=0;
-        for(int i=1;i<=9;i++){
-            if(i>5){
+        int st = 0;
+        for (int i = 1; i <= 9; i++) {
+            if (i > 5) {
                 --st;
-            }else{
-                ++st;efrjv ekjvneklvjn rev livj
+            } else {
+                ++st;
             }
 
-            for(int j=0;j<st;j++){
+            for (int j = 0; j < st; j++) {
                 System.out.print("* ");
             }
             System.out.println(" ");
-            
-
 
         }
-   }
+    }
 
 }
