008793cca1aaa92a31df9e5441a917ceee4a1c3e
inthrakumar <inthrak04@gmail.com>
2022-12-22 18:09:52 +0530

    correction

--- codes/java/a.java
+++ codes/java/a.java
@@ -3,7 +3,7 @@ public class a{
         int st=0;
         for(int i=1;i<=9;i++){
             if(i>5){
-                --st;//this is a edit 
+                --st;
             }else{
                 ++st;
             }
