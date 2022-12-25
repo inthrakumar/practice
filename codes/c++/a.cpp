359e5922e1c396440dba2776c8332ca9cc9729a6
inthrakumar <inthrak04@gmail.com>
2022-12-25 18:47:22 +0530

    c++

---
+++ codes/c++/a.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    int x;
+    string ak;
+    cout << "enter the string \n";
+    getline(cin, ak);
+    cout << ak;
+    return 0;
+}
